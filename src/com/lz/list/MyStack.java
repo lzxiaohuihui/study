9a88e6142cda10990d1199e31ee1a978fd76cf59
小灰灰 <895827938@qq.com>
2019-11-18 22:49:36 +0800

    顺序栈

--- src/com/lz/list/MyStack.java
+++ src/com/lz/list/MyStack.java
@@ -5,38 +5,37 @@ import java.util.ArrayList;
 //
 public class MyStack {
     ArrayList<Object> stack ;
-    Object base;
-    Object top;
+    //栈底下标
+    int base;
+    //栈顶下表
+    int top;
+    //栈大小
     int size;
 
     public MyStack(){
         this.stack = new ArrayList<>();
         this.size=0;
-        this.base = null;
-        this.top = null;
+        this.base = 0;
+        this.top = 0;
     }
 
     public void initStack(){
         this.stack = new ArrayList<>();
         this.size=0;
-        this.base = null;
-        this.top = null;
+        this.base = 0;
+        this.top = 0;
     }
     public void destroyStack(){
+        initStack();
         this.stack = null;
-        this.size=0;
-        this.base = null;
-        this.top = null;
     }
 
     public void clearStack(){
-        this.size=0;
-        this.base = null;
-        this.top = null;
+        initStack();
     }
 
     public boolean stackEmpty(){
-        if (this.top==null||this.base==null){
+        if (top==base){
             return true;
         }
         return false;
@@ -45,37 +44,52 @@ public class MyStack {
         return this.stack.size();
     }
     public Object getTop(){
-        return this.top;
+        return this.stack.get(size-1);
     }
     public void push(Object e){
         this.stack.add(e);
         this.size = this.stack.size();
-        this.base = this.stack.get(0);
-        this.top = this.stack.get(size-1);
+        this.top = this.size;
     }
     public Object pop(){
         Object e = null;
-        if (stackEmpty()){
+        if (!stackEmpty()){
             e = stack.remove(this.size-1);
         }
         this.size = this.stack.size();
-        this.base = this.stack.get(0);
-        this.top = this.stack.get(size-1);
+        this.base = 0;
+        this.top = this.size;
         return e;
     }
     public void stackTraverse(){
         System.out.println(this.stack);
     }
 
+    //十进制数转n进制数
+    public void conversion(int data, int n){
+        initStack();
+        while (data>0){
+            push(data%8);
+            data = data/8;
+            if (data==0){
+                break;
+            }
+        }
+        while (!stackEmpty()){
+            System.out.println(pop());
+        }
+    }
+
     public static void main(String[] args) {
         MyStack myStack = new MyStack();
-        myStack.push(1);
-        myStack.push(3);
-        myStack.push(5);
-        myStack.push(2);
-        myStack.pop();
-        System.out.println(myStack.stackLength());
-        myStack.stackTraverse();
+//        myStack.push(1);
+//        myStack.push(3);
+//        myStack.push(5);
+//        myStack.push(2);
+//        myStack.pop();
+//        System.out.println(myStack.stackLength());
+//        myStack.stackTraverse();
+        myStack.conversion(1348,8);
     }
 
 }
